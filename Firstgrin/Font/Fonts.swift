028e1dd3cdf470d93869e8c012204385aeb323e5
SabbirHossain07 <sabbirsopnil07@gmail.com>
2022-11-14 04:46:00 +0600

    Fonts

--- Firstgrin/Font/Fonts.swift
+++ Firstgrin/Font/Fonts.swift
@@ -10,5 +10,10 @@ import SwiftUI
 struct Fonts{
     static let WorkSansBold = "WorkSans-Bold"
     static let WorkSansMedium = "WorkSans-Medium"
+    static let WorkSansThin = "WorkSans-Thin"
+    static let WorkSansRegular = "WorkSans-Regular"
+    static let WorkSansLight = "WorkSans-Light"
+    static let WorkSansSemibold = "WorkSans-SemiBold"
+    static let WorkSansBlack = "WorkSans-Black"
 }
 
